56637b36f89ac95bdcb9340c136a1f4277240139
Monkey4squirrel <114167075+Monkey4squirrel@users.noreply.github.com>
2024-06-24 08:43:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,12 +9,23 @@ const caixaResultado = document.querySelector(".caixa-resultado”);
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
-        enunciado: "Pergunta 1”,
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-    },
-    {
-        enunciado: "Pergunta 2”,
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-    },
-];
+{
+enunciado: “Assim que saiu da escola, você se
+depara com uma nova tecnologia: um chat que consegue
+responder a todas as dúvidas que uma pessoa pode ter.
+Além disso, o chat também gera imagens e áudios
+hiper-realistas. Qual o seu primeiro pensamento?”, alternativas: [
+{
+texto: “Isso é assustador!”,
+
+afirmacao: “afirmação”
+
+},
+{
+texto: “Isso é maravilhoso!”,
+
+afirmacao: “afirmação”
+
+}
+]
+},
